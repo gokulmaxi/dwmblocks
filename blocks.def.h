89cab97b14ee4f83cc56243b8390868b586effb8
gokul <gokulakams@gmail.com>
2021-07-04 12:21:35 +0530

    added battery and storage

--- blocks.def.h
+++ blocks.def.h
@@ -4,6 +4,9 @@ static const Block blocks[] = {
 	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
 	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+
+	{"",	"db-battery",	5,	3},
+	{"",	"db-disk",	60,	1}
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
