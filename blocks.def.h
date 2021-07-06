0603bb56116ee80b77c98ad1e61f3cd67ee0f7ae
gokul <gokulakams@gmail.com>
2021-07-06 11:34:14 +0530

    readded battery module

--- blocks.def.h
+++ blocks.def.h
@@ -1,12 +1,14 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+	/* {"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0}, */
 
 	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
 
-	{"",	"db-battery",	5,	3},
-	{"",	"db-disk",	60,	1}
+	{"",	"db-battery",	5,	1},
+	{"",	"db-disk",	    60,	2},
+	{"",	"db-volume",	60,	3},
+    {"",    "db-nettraf",   1,  4}
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
